afe6dc990fd9a7e6c5a9bca7d5412db13cbde6f0
Fening Manuel <feningmanuel@gmail.com>
2025-11-24 20:04:46 +0000

    update

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,22 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
+const base = '/to-do-list/'
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     tailwindcss(),
-    react()],
-    base: '/to-do-list/',
+    react(),
+    {
+      name: 'html-transform',
+      transformIndexHtml(html) {
+        return html.replace(
+          /href="\.\/favicon-32x32\.png"/,
+          `href="${base}favicon-32x32.png"`
+        )
+      }
+    }
+  ],
+    base: base,
 })
